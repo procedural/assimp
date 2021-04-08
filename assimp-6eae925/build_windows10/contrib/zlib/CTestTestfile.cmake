520f5bc82c1513e3711685ec47760331d45ce09a
Constantine Tarasenkov <iamvfx@gmail.com>
2021-04-08 21:38:35 +0300

    Compile on Windows 10 with Microsoft (R) C/C++ Optimizing Compiler Version 19.28.29337 for x64 (Microsoft Visual Studio 2019 Version 16.8.6)

---
+++ assimp-6eae925/build_windows10/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/iamvf/Desktop/assimp_binaries_ubuntu_16_04_and_windows_10/assimp-6eae925/contrib/zlib
+# Build directory: C:/Users/iamvf/Desktop/assimp_binaries_ubuntu_16_04_and_windows_10/assimp-6eae925/build_windows10/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
