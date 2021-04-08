8bca2ad68fedef782e3dfb16a3ba4ee6528aed15
Constantine Tarasenkov <iamvfx@gmail.com>
2021-04-08 18:36:26 +0300

    Compile on Ubuntu 16.04

---
+++ assimp-4.1.0/build/contrib/irrXML/CMakeFiles/IrrXML.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/constantine/Desktop/assimp5_binaries_ubuntu_16_04_and_windows_10/assimp-4.1.0/contrib/irrXML/irrXML.cpp" "/home/constantine/Desktop/assimp5_binaries_ubuntu_16_04_and_windows_10/assimp-4.1.0/build/contrib/irrXML/CMakeFiles/IrrXML.dir/irrXML.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ASSIMP_BUILD_NO_C4D_IMPORTER"
+  "ASSIMP_BUILD_NO_EXPORT"
+  "ASSIMP_BUILD_NO_OWN_ZLIB"
+  "OPENDDL_NO_USE_CPP11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../include"
+  "."
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
