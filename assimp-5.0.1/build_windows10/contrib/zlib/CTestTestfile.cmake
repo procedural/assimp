4d1e83d17ba1407425bed21fbd01e32dad05d00b
Constantine Tarasenkov <iamvfx@gmail.com>
2021-04-08 13:03:11 +0300

    Recompile on Windows 10 again

---
+++ assimp-5.0.1/build_windows10/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/iamvf/Documents/Archive/Github/assimp5_binaries_ubuntu_16_04_and_windows_10/assimp-5.0.1/contrib/zlib
+# Build directory: C:/Users/iamvf/Documents/Archive/Github/assimp5_binaries_ubuntu_16_04_and_windows_10/assimp-5.0.1/build_windows10/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
